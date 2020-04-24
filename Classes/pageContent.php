b5ed0e1b6d23847610197d4df2081fcef7dffa9c
Your Name <mdarifahmed01@gmail.com>
2020-04-24 11:33:02 +0600

    First Commit

---
+++ Classes/pageContent.php
@@ -0,0 +1,12 @@
+<?php
+
+    class pageContent{
+
+        public function getIndexHeader(){
+            require_once('inc/index_header.php');
+        }
+
+        public function getFooter(){
+            require_once('inc/footer.php');
+        }
+    }
---
+++ functions/frontend.php
@@ -0,0 +1,12 @@
+<?php
+
+    /***************************************************************************
+    This page maintain all the functions for the frontend
+    ***************************************************************************/
+
+    //page joining class
+    require_once 'Classes/pageContent.php';
+
+
+    // object of pageContent class
+    $page_content = new pageContent();
