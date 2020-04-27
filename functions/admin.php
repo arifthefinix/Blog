844290f92a0facb7eb810efa8516a626e80e80b9
Your Name <mdarifahmed01@gmail.com>
2020-04-27 15:12:08 +0600

    Database Connection

---
+++ functions/admin.php
@@ -0,0 +1,16 @@
+<?php
+
+    /***************************************************************************
+    This page maintain all the functions for the Admin
+    ***************************************************************************/
+
+    //page joining class
+    require_once '../Config/config.php';
+    require_once '../Classes/pageContent.php';
+    require_once '../Classes/database.php';
+
+
+    // object of pageContent class
+    $page_content = new pageContent();
+    // object of Database class
+    $db = new Database();
